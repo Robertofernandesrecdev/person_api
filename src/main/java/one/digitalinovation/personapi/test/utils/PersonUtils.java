dabbf65d0e769b3c42c86092764ab80c4e2f6830
Roberto <robertofernandes199@hotmail.com>
2021-12-23 20:21:47 -0300

    final commit

---
+++ src/main/java/one/digitalinovation/personapi/test/utils/PersonUtils.java
@@ -0,0 +1,38 @@
+package one.digitalinovation.personapi.test.utils;
+
+
+import one.digitalinovation.personapi.dto.request.PersonDTO;
+import one.digitalinovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Roberto";
+    private static final String LAST_NAME = "Fernandes";
+    private static final String CPF_NUMBER = "042.111.777-55";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .fristName(FIRST_NAME)
+                .LastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("04-04-2010")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
---
+++ src/main/java/one/digitalinovation/personapi/test/utils/PersonapiApplicationTests.java
@@ -0,0 +1,15 @@
+package one.digitalinovation.personapi.test.utils;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
